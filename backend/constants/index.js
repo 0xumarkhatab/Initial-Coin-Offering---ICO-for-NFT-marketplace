a2b8cd2c546da38535af05ff9a95ae367b74d1f8
Umar Esso <umarkhatab467@gmail.com>
2022-08-11 16:20:58 +0500

    Added backend

---
+++ backend/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x23415E997ed6a1C9328F045457761587693a2521";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
